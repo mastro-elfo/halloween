fbbe254706773e34add9ccf7b850b041115baefe
Francesco <francesco.209@gmail.com>
2025-07-04 00:41:25 +0200

    🎉 New version 3.0 with React!

---
+++ src/components/Countdown/Countdown.tsx
@@ -0,0 +1,93 @@
+import { Box, Stack, Typography, useTheme } from "@mui/material";
+
+type CountdownProps = {
+  days: number;
+};
+
+export default function Countdown({ days }: CountdownProps) {
+  const theme = useTheme();
+  const asString = String(days).padStart(3, "0");
+  const hundreds = asString.at(0);
+  const tens = asString.at(1);
+  const units = asString.at(2);
+  const fontSize = theme.typography.h2.fontSize;
+  return (
+    <Stack
+      direction="row"
+      spacing={1}
+      p={1}
+      borderRadius={theme.shape.borderRadius}
+      sx={{
+        fontSize,
+        backgroundColor: theme.palette.primary.dark,
+      }}
+    >
+      <Box
+        sx={{
+          background:
+            "linear-gradient(to top, black, antiquewhite 5%, white 49%, black, white 51%, antiquewhite 95%, black)",
+          height: "1.5em",
+          width: "1em",
+          borderRadius: [
+            `${theme.shape.borderRadius}px`,
+            0,
+            0,
+            `${theme.shape.borderRadius}px`,
+          ].join(" "),
+          boxShadow: "",
+        }}
+      >
+        <Typography
+          fontSize={fontSize}
+          fontFamily="Manufacturing Consent"
+          color="black"
+          align="center"
+        >
+          {hundreds}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "white",
+          height: "1.5em",
+          width: "1em",
+          background:
+            "linear-gradient(to top, black, antiquewhite 5%, white 49%, black, white 51%, antiquewhite 95%, black)",
+        }}
+      >
+        <Typography
+          fontSize={fontSize}
+          fontFamily="Manufacturing Consent"
+          color="black"
+          align="center"
+        >
+          {tens}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "white",
+          background:
+            "linear-gradient(to top, black, antiquewhite 5%, white 49%, black, white 51%, antiquewhite 95%, black)",
+          height: "1.5em",
+          width: "1em",
+          borderRadius: [
+            0,
+            `${theme.shape.borderRadius}px`,
+            `${theme.shape.borderRadius}px`,
+            0,
+          ].join(" "),
+        }}
+      >
+        <Typography
+          fontSize={fontSize}
+          fontFamily="Manufacturing Consent"
+          color="black"
+          align="center"
+        >
+          {units}
+        </Typography>
+      </Box>
+    </Stack>
+  );
+}
